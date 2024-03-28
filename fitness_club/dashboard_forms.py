47d86f3a95c315dfee84777df009d191f385567f
Evan Moore <eqmoore1@gmail.com>
2024-03-28 19:07:41 -0400

    Added a height form so a member can update their height. Also updated fields in the LogWeightForm to have the correct datatype.

--- fitness_club/dashboard_forms.py
+++ fitness_club/dashboard_forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import DateField, DecimalField
 from wtforms.validators import DataRequired
 
 
 class LogWeightForm(FlaskForm):
-    weight = StringField('Weight', validators=[DataRequired()])
-    date = StringField('Date', validators=[DataRequired()])
+    weight = DecimalField('Weight', validators=[DataRequired()])
+    date = DateField('Date', validators=[DataRequired()])
+
+
+class HeightForm(FlaskForm):
+    height = DecimalField('Height', validators=[DataRequired()])
